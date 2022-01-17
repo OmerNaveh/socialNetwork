125d6ae26cc9b649a117fc46628b6ef4bf32a5c1
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2022-01-17 16:45:31 +0200

    added some basic styling

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Messages from "./components/Messages";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Settings from "./components/Settings";
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <div className="App"></div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ front/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="content">
+      In Home
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+    </div>
+  );
+}
